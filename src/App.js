790539be653efa604413e1923a8fe7b06bee6cb7
AKHILRAJ1995 <akhilraj661@gmail.com>
2023-02-14 18:57:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Todoapp from './Component/Todoapp';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+
+          <Todoapp/>
+
+      </div>
+    )
+  }
+}
